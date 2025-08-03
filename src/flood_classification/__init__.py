df88e3a25f7b3b00c7bfeec080f094f144b79d3e
andrenogueira88 <andrenogsousa@gmail.com>
2025-08-03 19:37:12 -0300

    including extra fields and resolution matching

--- src/flood_classification/__init__.py
+++ src/flood_classification/__init__.py
@@ -1,2 +1,9 @@
 from .load import load_flood_points, load_raster
-from .process import check_resolution_and_crs, raster_to_dataframe, transform_to_raster
+from .process import (
+    check_resolution_and_crs,
+    raster_to_dataframe,
+    transform_to_raster,
+    create_final_dataframe,
+    calculate_distance_to_features,
+    transform_vector_to_binary_raster
+)
